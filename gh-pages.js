a420d1ea7ca3ba35f1c93387f80db80e0b8e87d7
IAL32 <adrian.castro@tacto.ai>
2024-02-08 09:01:58 +0100

    fix: add `.nojekyll` to build

--- gh-pages.js
+++ gh-pages.js
@@ -1,18 +1,20 @@
 import { publish } from 'gh-pages';
+import fs from 'fs';
 
 publish(
-  'build', // path to public directory
-  {
-    branch: 'gh-pages',
-    repo: 'https://github.com/IAL32/calcolo-mutuo-casa.git', // Update to point to your repository
-    user: {
-      name: 'IAL32', // update to use your name
-      email: '562969+IAL32@users.noreply.github.com' // Update to use your email
+    'build', // path to public directory
+    {
+        nojekyll: true,
+        branch: 'gh-pages',
+        repo: 'https://github.com/IAL32/calcolo-mutuo-casa.git', // Update to point to your repository
+        user: {
+            name: 'IAL32', // update to use your name
+            email: '562969+IAL32@users.noreply.github.com' // Update to use your email
+        },
+        dotfiles: true,
+        cname: 'mutuo.adct.it'
     },
-    dotfiles: true,
-    cname: 'mutuo.adct.it'
-  },
-  () => {
-    console.log('Deploy Complete!');
-  }
+    () => {
+        console.log('Deploy Complete!');
+    }
 );
